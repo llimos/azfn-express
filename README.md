41be9fa5d178a7aeaa5a7a0736f20d1b3c955c50
Shimon Lyons <shimonl@duck.com>
2024-05-23 15:47:07 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # azfn-express
-Adapter for using an Express app with Azure Functions
+This package allows you to expose an [ExpressJS](https://expressjs.com) application as a serverless app using Azure Functions.
+
+All Express routing and middleware is supported. A single Azure function handles your entire API.
+
+Using the new v4 programming model, natively streamed requests and responses are also supported, just as if you were using standalone Express.
+
+## Requirements
+* Your Azure Functions app is using the new v4 programming model
+
+## Quick Start
+1. Add this package to your app: 
+`yarn add azfn-express` or `npm install azfn-express`
+2. Use the `register` function to register your Express application with Azure Functions. Make sure the file this is in gets imported eventually from your `main` entrypoint.
+```ts
+import { register } from 'azfn-express';
+import { myExpressApp } from './app';
+
+register(myExpressApp);
+```
+This will register your app with sensible defaults, as well as enable HTTP Streaming in your Azure app. A function called 'Api' will be created, that will handle all incoming requests.
+
+*Note:* By default, Azure Functions has a base URL path of `/api`. The adapter does **not** strip this out, so your Express app routing needs to take the prefix into account.
+
+## Detailed Instructions
+For more customization, you may provide the name of the app, or a configuration object that will be passed straight to the Azure registration function, or both.
+```ts
+register(myExpressApp, 'MyApi', {route: '/myapi/{*segments}'})
+```
+```ts
+register(myExpressApp, {
+    methods: ['GET'], 
+    authLevel: 'function'
+});
+```
+For even more control, instead of using `register`, you can use `createHandler` which will return the handler function for you to register yourself.
+```ts
+import { app } from '@azure/functions';
+import { createHandler } from 'azfn-express';
+import { myExpressApp } from './app';
+
+app.setup({ enableHttpStream: true })
+app.http('MyApi', {
+    handler: createHandler(myExpressApp),
+    route: '{*segments}'
+});
+```
+
+## Inside Express
+Request bodies are always provided to Express as a raw stream only. You will need to add Express middlewares for parsing JSON and so on. This package does not use the internal Azure methods for body parsing.
+
+Two extra properties will be available on the Express request object:
+* `context` - this is the Azure Functions invocation context, used for logging and so on
+* `user` - if using Azure authentication, this will be the user object, if there is one
+
+See the Express documentation for how to handle streaming requests and responses, if you wish to do so. You may also use the Express `send` method, or any other Express funcionality.
---
+++ src/index.ts
@@ -0,0 +1,159 @@
+import { IncomingMessage, ServerResponse } from 'node:http';
+import { Readable } from 'node:stream';
+import { ReadableStream, ReadableStreamController } from 'node:stream/web';
+import {
+    app,
+    HttpResponse,
+    HttpFunctionOptions,
+    type HttpHandler,
+    type HttpRequest,
+    type HttpResponseInit,
+    type InvocationContext,
+    HttpRequestUser,
+} from '@azure/functions';
+import type { Application } from 'express';
+
+
+declare module "node:http" {
+    interface IncomingMessage {
+        context: InvocationContext;
+        user: HttpRequestUser | null;
+    }
+    interface ServerResponse<Request extends IncomingMessage = IncomingMessage> {
+        _headerSent: boolean;
+    }
+}
+
+export function createExpressHandler(app: Application): HttpHandler {
+   return function expressHandler(
+      request: HttpRequest,
+      context: InvocationContext,
+   ): Promise<HttpResponseInit> {
+      return new Promise((resolve, reject) => {
+         // Create an IncomingMessage for Express, with the request body as the stream
+         let req: IncomingMessage;
+         if (request.body)
+            req = Object.setPrototypeOf(
+               Readable.fromWeb(request.body),
+               IncomingMessage.prototype,
+            );
+         else {
+            //@ts-ignore Typings don't allow null socket but it does work
+            req = new IncomingMessage(null);
+            req.push(null);
+         }
+         req.on('error', reject);
+         req.method = request.method;
+         req.headers = Object.fromEntries(request.headers.entries());
+         req.url = request.url;
+         req.user = request.user;
+         // Add Azure context
+         req.context = context;
+
+         // Create a ServerResponse for Express
+         const res = new ServerResponse(req).on('error', reject);
+
+         // Readable stream to return to Azure
+         let responseStreamController: ReadableStreamController<Buffer>;
+
+         // Override the part that actually sends the data, to send it to Azure instead
+         // We need to override both of these - `write` is used for streaming and `_send` for Express's `send`
+         res.write = function write(
+            this: ServerResponse,
+            data,
+            encoding?: BufferEncoding | undefined | ((error?: Error | null | undefined) => void),
+            callback?: (error?: Error | null | undefined) => void,
+         ) {
+            // This is from the original function
+            if (typeof encoding === 'function') {
+               callback = encoding;
+               encoding = undefined;
+            }
+
+            // When Express decides to write the headers, return the response to Azure including the body as a stream
+            if (!this._headerSent) {
+               // Return to Azure immediately
+               // If there is a response body, it will be streamed
+               resolve(
+                  new HttpResponse({
+                     status: this.statusCode,
+                     headers: this.getHeaders() as Record<
+                        string,
+                        string | string[]
+                     >,
+                     body: data
+                        ? new ReadableStream<Buffer>({
+                             start: controller =>
+                                (responseStreamController = controller),
+                          })
+                        : null,
+                  }),
+               );
+
+               this._headerSent = true;
+            }
+
+            // Write the data, if any
+            if (data) {
+               responseStreamController.enqueue(data);
+            }
+
+            // Confirm that the data has been processed
+            callback?.();
+
+            return true;
+         };
+
+         res.end = function end(
+            this: ServerResponse,
+            data?,
+            encoding?: BufferEncoding | ((error?: Error | null | undefined) => void),
+            callback?: (error?: Error | null | undefined) => void,
+         ) {
+            if (typeof data === 'function') {
+               callback = data;
+               data = null;
+               encoding = undefined;
+            } else if (typeof encoding === 'function') {
+               callback = encoding;
+               encoding = undefined;
+            }
+
+            this.write(data, encoding as BufferEncoding);
+            responseStreamController?.close();
+
+            callback?.();
+
+            return this;
+         };
+
+         // Process the request
+         try {
+            app(req, res);
+         } catch (e) {
+            reject(e);
+         }
+      });
+   };
+}
+
+
+type Options = Omit<HttpFunctionOptions, 'handler'>;
+export function register(expressApp: Application, name?: string, options?: Options): void;
+export function register(expressApp: Application, options: Options): void;
+export function register(expressApp: Application, name?: string | Options, options?: Options): void {
+    if (typeof name !== 'string') {
+        options = name;
+        name = 'Api';
+    }
+
+    // Default route which can be overridden, and handler which cannot
+    const mergedOptions: HttpFunctionOptions = Object.assign(
+        { route: '{*segments}' }, 
+        options, 
+        {handler: createExpressHandler(expressApp)}
+    );
+
+    app.setup({ enableHttpStream: true });
+    app.http(name, mergedOptions)
+}
